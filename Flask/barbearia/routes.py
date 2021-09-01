2202b4ce9fd84d4363fd8c14ee6462c7d0abecd5
Lucas <lucasalvescampos@gmail.com>
2021-09-01 20:28:14 -0300

    adicionado pagina de cadastro e conexão com o banco de dados

--- Flask/barbearia/routes.py
+++ Flask/barbearia/routes.py
@@ -1,5 +1,9 @@
 from barbearia import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+
+from Flask.run import Cadastrousuario
+from Flask.run import db
+
 
 @app.route('/')
 def menu_page():
@@ -8,3 +12,22 @@ def menu_page():
 @app.route('/home')
 def home_page():
     return render_template('menu.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route('/cadastrousuario', methods=['GET', 'POST'])
+def cadastrousuario():
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        senha = request.form.get('senha')
+        email = request.form.get('email')
+        cpf = request.form.get('cpf')
+        telefone = request.form.get('telefone')
+        datanascimento = request.form.get('datanascimento')
+        if nome:
+            f = Cadastrousuario(nome, senha, email, cpf, telefone, datanascimento)
+            db.session.add(f)
+            db.session.commit()
+    return redirect(url_for('menu_page'))
--- Flask/run.py
+++ Flask/run.py
@@ -1,4 +1,32 @@
 from barbearia import app
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://fnlwdzmflpxpby:10500a639e7edb5a403284b9334bbbea6f287d16c33646ef901e2d1cd6b14a64@ec2-34-194-14-176.compute-1.amazonaws.com:5432/daq0i5hffjvjft'
+
+db = SQLAlchemy(app)
+
+class Cadastrousuario(db.Model):
+    __tablename__ = "usuarioscadastrados"
+    _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(50))
+    senha = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    cpf = db.Column(db.String(50))
+    telefone = db.Column(db.String(15))
+    datanascimento = db.Column(db.String(10))
+
+    def __init__(self, nome, senha, email, cpf, telefone, datanascimento):
+        self.nome = nome
+        self.senha = senha
+        self.email = email
+        self.cpf = cpf
+        self.telefone = telefone
+        self.datanascimento = datanascimento
+
+db.create_all()
+
