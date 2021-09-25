ccd24837183af103bc62d9800cc9b3db51454b5e
FabioMarquesArao <fabio.arao@outlook.com>
2021-09-25 02:57:28 -0300

    acerto

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 from barbearia import app
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    app.run(port=4800, debug=True)
-=======
+    
     app.run(port=4800, debug=True)
     
->>>>>>> origin/master
